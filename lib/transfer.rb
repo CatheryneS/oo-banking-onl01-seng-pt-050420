cab470925758e3823dfd8f543f538a5a4765d238
Catheryne Small <catheryn.small@gmail.com>
2020-06-06 17:17:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,8 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
+    elsif @sender.valid? && @sender.balance < @amount && @status == 'pending'
+      @status = 
     end
   end
   
