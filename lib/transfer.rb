bc38bcc626aaad765d5f22b25bcd83a3c6121869
Catheryne Small <catheryn.small@gmail.com>
2020-06-06 16:27:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,10 @@
 class Transfer
+  attr_accessor :status
   attr_reader :sender, :receiver
   
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
+    @status = "pending"
   end
 end
