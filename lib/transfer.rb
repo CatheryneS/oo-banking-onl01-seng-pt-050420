9f4b0178985012b3299440fcbb345983d2a1189f
Catheryne Small <catheryn.small@gmail.com>
2020-06-06 17:45:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,13 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && self.status == "pending"
+    if valid? && self.status == "pending" && @sender.balance > @amount
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @amount = 0.0 && self.status = "complete"
     end
   end
   
-  def reject 
-    if 
+  
+    
 end
