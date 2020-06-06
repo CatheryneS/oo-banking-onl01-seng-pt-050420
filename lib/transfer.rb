85640d44dcf4e7a6075cf3b4b4f6dee5d0dce116
Catheryne Small <catheryn.small@gmail.com>
2020-06-06 17:14:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    
+    if @sender.valid?
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
