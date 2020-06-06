2d6e80ac4fb4c994e98a1c8667146192351376ab
Catheryne Small <catheryn.small@gmail.com>
2020-06-06 16:47:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount
     @amount = 0
-    if @sender.valid? == false || @receiver.valid == false 
+    if @sender.valid? == false || @receiver.valid? == false 
       @status = "Transaction rejected. Please check your account balance."
     else 
       @status = "complete"
