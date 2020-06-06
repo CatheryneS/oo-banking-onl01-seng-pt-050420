35ecd2973adac7060e5d4e3ff3b1a143b588e22a
Catheryne Small <catheryn.small@gmail.com>
2020-06-06 17:42:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,10 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance < @amount
-      return "Transaction rejected. Please check your account balance."
-    else
+    if valid? && self.status == "pending"
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @amount = 0.0 && self.status = "complete"
     end
   end
-  
 end
